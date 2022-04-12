880cfdd7ff3565339dfcec4cc3242d5869a5acdc
Pauline <2245718@cgodin.qc.ca>
2022-04-11 20:32:34 -0400

    Bonjour

--- src/gouv/cegep/gg/main/Calculer.java
+++ src/gouv/cegep/gg/main/Calculer.java
@@ -23,5 +23,8 @@ public class Calculer {
         calculeleNombre.afficher(addition, "addition");
         calculeleNombre.afficher(soustraction, "soustraction");
 
+        System.out.println("Bonjour Git");
+
     }
+
 }
